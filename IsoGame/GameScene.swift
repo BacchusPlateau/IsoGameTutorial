6caa3c70c1e9c854368108cd744324d3eaae6dd2
Bret Williams <bret@brets-mbp.tx.rr.com>
2018-05-04 20:48:13 -0500

    Iso Game part 1 tutorial

--- IsoGame/GameScene.swift
+++ IsoGame/GameScene.swift
@@ -1,89 +1,113 @@
-//
-//  GameScene.swift
-//  IsoGame
-//
-//  Created by Bret Williams on 5/4/18.
-//  Copyright © 2018 Bret Williams. All rights reserved.
-//
-
 import SpriteKit
-import GameplayKit
+
+enum Tile: Int {
+    
+    case Ground
+    case Wall
+    
+    var description:String {
+        switch self {
+        case .Ground:
+            return "Ground"
+        case .Wall:
+            return "Wall"
+        }
+    }
+    
+    var image:String {
+        switch self {
+        case .Ground:
+            return "ground"
+        case .Wall:
+            return "wall"
+            
+        }
+    }
+}
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //1
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
+    //2
+    let view2D:SKSpriteNode
+    let viewIso:SKSpriteNode
+    
+    //3
+    let tiles = [
+        [1, 1, 1, 1, 1, 1],
+        [1 ,0, 0, 0, 0, 1],
+        [1 ,0, 0, 0, 0, 1],
+        [1 ,0, 0, 0, 0, 1],
+        [1 ,0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1]
+    ]
+    let tileSize = (width:32, height:32)
+    
+    //4
+    override init(size: CGSize) {
+        
+        view2D = SKSpriteNode()
+        viewIso = SKSpriteNode()
+        
+        super.init(size: size)
+        self.anchorPoint = CGPoint(x:0.5, y:0.5)
+    }
+    
+    //5
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let deviceScale = self.size.width/667
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        view2D.position = CGPoint(x:-self.size.width*0.45, y:self.size.height*0.17)
+        view2D.xScale = deviceScale
+        view2D.yScale = deviceScale
+        addChild(view2D)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        viewIso.position = CGPoint(x:self.size.width*0.12, y:self.size.height*0.12)
+        viewIso.xScale = deviceScale
+        viewIso.yScale = deviceScale
+        addChild(viewIso)
+        placeAllTiles2D()
+        print ("did move")
+    }
+    
+    func placeTile2D(image:String, withPosition:CGPoint) {
+    
+    let tileSprite = SKSpriteNode(imageNamed: image)
+    
+    tileSprite.position = withPosition
+    
+    tileSprite.anchorPoint = CGPoint(x:0, y:0)
+    
+    view2D.addChild(tileSprite)
+    
+    }
+    
+
+    
+    func placeAllTiles2D() {
+        
+        for i in 0..<tiles.count {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            let row = tiles[i];
+            
+            for j in 0..<row.count {
+                let tileInt = row[j]
+                
+                //1
+                let tile = Tile(rawValue: tileInt)!
+                
+                //2
+                let point = CGPoint(x: (j*tileSize.width), y: -(i*tileSize.height))
+                
+                placeTile2D(image: tile.image, withPosition:point)
+            }
+            
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
